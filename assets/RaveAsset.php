a7db9d81f61513a87318bfe756177235912eaace
Rave <rave@iMac-Rave.local>
2020-10-12 18:26:29 +0300

    Change Assets

---
+++ assets/RaveAsset.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ravesoft\assets;
+
+use yii\web\AssetBundle;
+use yii\web\JqueryAsset;
+
+/**
+ * Class RaveAsset
+ * 
+ * @package ravesoft\core
+ */
+class RaveAsset extends AssetBundle
+{
+
+    public function init()
+    {
+        $this->sourcePath = __DIR__ . '/admin';
+
+        $this->js = [
+            'js/admin.js',
+        ];
+
+        $this->css = [
+            'css/admin.css',
+            'css/widget.css',
+            'css/styler.css',
+        ];
+
+        $this->depends = [
+            JqueryAsset::className(),
+            'yii\web\YiiAsset',
+            'yii\bootstrap\BootstrapAsset',
+            'rmrevin\yii\fontawesome\AssetBundle',
+        ];
+
+        parent::init();
+    }
+}
